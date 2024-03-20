6e8d799e03acadda8787274455f2a0f76ad9cd03
bortcrab <bortcrab@gmail.com>
2024-03-19 22:17:14 -0700

    Creé el proyecto

---
+++ Ejteechi/src/main/java/main/Ejteechi.java
@@ -0,0 +1,16 @@
+package main;
+
+import presentacion.FrmLogin;
+
+
+/**
+ *
+ * @author jorge
+ */
+public class Ejteechi {
+
+    public static void main(String[] args) {
+        FrmLogin frmLogin = new FrmLogin();
+        frmLogin.setVisible(true);
+    }
+}
