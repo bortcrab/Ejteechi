e62fd7b8b6bcdd5ed510d9b6bfb6efc32243b32b
bortcrab <bortcrab@gmail.com>
2024-04-28 20:48:12 -0700

    Eliminé unas dependencias que estaban de más

--- ObjetosNegocio/src/main/java/objetosNegocio/IMapaBO.java
+++ ObjetosNegocio/src/main/java/objetosNegocio/IMapaBO.java
@@ -1,7 +1,18 @@
 package objetosNegocio;
 
+import dtos.LineaDTO;
 import dtos.MapaDTO;
+import dtos.RutaDTO;
+import persistencia.Linea;
+import persistencia.Mapa;
+import persistencia.Ruta;
 
 public interface IMapaBO {
     public MapaDTO obtenerMapa() throws ObjetosNegocioException;
+    
+    public MapaDTO convertirMapaDTO(Mapa mapa);
+    
+    public LineaDTO convertirLineaDTO(Linea linea);
+    
+    public RutaDTO convertirRutaDTO(Ruta ruta);
 }
